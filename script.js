00b8d1bbe5273bb650cbbd6e5da7a48ee3772760
recharge-network <myrechargenetwork@gmail.com>
2025-07-24 09:28:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA9KS6E7oWd2aNumYPRkTpCvl_Sqb1TZtE",
   authDomain: "mlm-network-157e8.firebaseapp.com",
   projectId: "mlm-network-157e8",
-  storageBucket: "mlm-network-157e8.firebasestorage.app",
+  storageBucket: "mlm-network-157e8.appspot.com",  // ✅ corrected
   messagingSenderId: "433192039581",
   appId: "1:433192039581:web:42d02b965870e4a8969074"
 };

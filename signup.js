be65e2ca2dc5da8004b92b29b8b4a37646075adf
recharge-network <myrechargenetwork@gmail.com>
2025-07-24 09:06:22 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -3,7 +3,23 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIza....",             // 🔁 apna config yahan bhi paste karo
+  apiKey: "AIza....",             // // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9KS6E7oWd2aNumYPRkTpCvl_Sqb1TZtE",
+  authDomain: "mlm-network-157e8.firebaseapp.com",
+  projectId: "mlm-network-157e8",
+  storageBucket: "mlm-network-157e8.firebasestorage.app",
+  messagingSenderId: "433192039581",
+  appId: "1:433192039581:web:42d02b965870e4a8969074"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   authDomain: "mlm-network.firebaseapp.com",
   projectId: "mlm-network",
   storageBucket: "mlm-network.appspot.com",

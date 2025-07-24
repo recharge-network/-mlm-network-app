bb67971068dcbffae786078237fd19db1fb7a07f
recharge-network <myrechargenetwork@gmail.com>
2025-07-24 08:51:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function login() {
     alert("Please enter email and password");
   }
 }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9KS6E7oWd2aNumYPRkTpCvl_Sqb1TZtE",
+  authDomain: "mlm-network-157e8.firebaseapp.com",
+  projectId: "mlm-network-157e8",
+  storageBucket: "mlm-network-157e8.firebasestorage.app",
+  messagingSenderId: "433192039581",
+  appId: "1:433192039581:web:42d02b965870e4a8969074"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
